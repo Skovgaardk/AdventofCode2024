b48ceb4d773675322c304a587ba2b6eb69417a03
Rasmus Skovgaard Pedersen <Rasmusskovgaardp@gmail.com>
2024-12-02 12:49:33 +0100

    Finnished day 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode.Day1;
+using AdventOfCode.Day2;
 
 namespace Advent_of_code_2024;
 
@@ -13,6 +14,13 @@ public class Program
         {
             case "1" :
                 new Day1().Run();
+                break;
+            case "2":
+                new Day2().Run();
+                break;
+            
+            default:
+                Console.WriteLine("No valid date selected");
                 break;
         }
     }
---
+++ day 2/day2solution.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+using Advent_of_code_2024.Utilities;
+
+namespace AdventOfCode.Day2
+{
+    public class Day2
+    {
+        private bool checkIfSafe(int[] line)
+        {
+
+
+            if (line.Length < 2) return true;
+
+            bool isIncreasing = line[1] > line[0];
+            bool isSafe = true;
+
+            for (int i = 1; i < line.Length; i++)
+            {
+                int difference = line[i] - line[i - 1];
+
+                if (isIncreasing)
+                {
+                    if (difference <= 0 || difference > 3)
+                    {
+                        isSafe = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (difference >= 0 || difference < -3)
+                    {
+                        isSafe = false;
+                        break;
+                    }
+                }
+
+            }
+
+            return isSafe;
+        }
+        public void Run()
+        {
+
+            var input = Utility.GetInput(2);
+            List<int[]> ints = new List<int[]>();
+            
+            foreach (string s in input)
+            {
+                int[] array = s.Split(" ").Select(Int32.Parse).ToArray();
+                ints.Add(array);
+            }
+            
+            int amountOfSafe = 0;
+
+            foreach (int[] line in ints)
+            {
+                if (checkIfSafe(line))
+                {
+                    amountOfSafe += 1;
+                }
+            }
+            
+            Console.WriteLine($"Part 1 amount of safe: {amountOfSafe}");
+
+
+            int amoutOfSafeIfOneIsAllowed = 0;
+            foreach (int[] line in ints)
+            {
+                if (checkIfSafe(line) || Enumerable.Range(0, line.Length) //Get range of 0 to the length of the list of integers +1
+                        //Take(i) gets the first i elements, Skip(i) skips the first i elements.
+                        //This way we can iterate over every combination of 1 skipped element to check if any of them returns true.
+                        .Any(i => checkIfSafe(line.Take(i).Concat(line.Skip(i + 1)).ToArray()))) 
+                {
+                    amoutOfSafeIfOneIsAllowed += 1;
+                }
+            }
+            
+            Console.WriteLine($"Part 2 amount of safe: {amoutOfSafeIfOneIsAllowed}");
+
+
+        }
+    }
+}
